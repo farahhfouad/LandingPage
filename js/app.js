b79282ae6f9f1fa1d9d8b94b164ec90d1acfaee6
farah <farahalaa1999@yahoo.com>
2020-06-20 16:10:09 +0200

    after solving viewport

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
 const navbar = document.querySelector('ul');
 const elements = document.querySelectorAll('.card');
 const container = document.querySelector('.content');
+// const containter = document.createDocumentFragment();
 
-const containter = document.createDocumentFragment();
+
 for (let i = 0; i < elements.length; i++) {
 
     const name = elements[i].querySelector('.card-body').getAttribute('id');
@@ -28,17 +29,30 @@ navbar.addEventListener('click', function (e) {
     }
     e.target.classList.add('active');
     const n = e.target.getAttribute('data');
-
     const elm = document.getElementById(`${n}`);
 
     elm.scrollIntoView();
 
 });
-container.addEventListener('mouseover', function (e) {
-    if (e.target.nodeName == "DIV") {
-        const ind = e.target.getAttribute('id');
-        console.log(ind);
-        if (ind != 'top' || ind !== null || ind !== 'null') {
+
+function inView(elem) {
+    const inV = elem.getBoundingClientRect();
+
+    return (
+        inV.top >= 0
+        &&
+        inV.top < (window.innerHeight || document.documentElement.clientHeight)
+
+    );
+}
+
+
+document.addEventListener('scroll', function (e) {
+    for (let i = 0; i < elements.length; i++) {
+
+        const chk = elements[i];
+        if (inView(chk)) {
+            const ind = chk.getAttribute('id')
             const navbarNew = document.querySelectorAll('li');
             for (let i = 0; i < navbarNew.length; i++) {
                 if (navbarNew[i].querySelector('a').getAttribute('data') == ind) {
@@ -48,8 +62,32 @@ container.addEventListener('mouseover', function (e) {
                     navbarNew[i].querySelector('a').classList.remove('active');
                 }
             }
-
         }
+
     }
 
 });
+
+
+
+//OLd method used to determine which element is in viewport by detemrining where the mouse is over
+
+// container.addEventListener('mouseover', function (e) {
+//     if (e.target.nodeName == "DIV") {
+//         const ind = e.target.getAttribute('id');
+//         console.log(ind);
+//         if (ind != 'top' || ind !== null || ind !== 'null') {
+//             const navbarNew = document.querySelectorAll('li');
+//             for (let i = 0; i < navbarNew.length; i++) {
+//                 if (navbarNew[i].querySelector('a').getAttribute('data') == ind) {
+//                     navbarNew[i].querySelector('a').classList.add('active');
+//                 }
+//                 else {
+//                     navbarNew[i].querySelector('a').classList.remove('active');
+//                 }
+//             }
+
+//         }
+//     }
+
+// });
---
+++ readme.md
@@ -0,0 +1,13 @@
+To run the project please open index.html file on your browser.
+
+Dependencies used: 
+-Bootstrap
+
+Resources used :
+-MDN web docs
+-JavascriptTutorial
+
+Description of functions:
+- a.scrollIntoView() --> used to show element a in the user's current view
+- elem.getBoundingClientRect() --> returns the element's x,y position along with its wifth, height, top, right, bottom, left
+
