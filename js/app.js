32fd13275511e8183ef61786c7b1c03f9666e5b7
farah <farahalaa1999@yahoo.com>
2020-06-21 11:07:04 +0200

    improved

--- js/app.js
+++ js/app.js
@@ -1,15 +1,17 @@
 const navbar = document.querySelector('ul');
 const elements = document.querySelectorAll('.card');
 const container = document.querySelector('.content');
-// const containter = document.createDocumentFragment();
+
 
 
 for (let i = 0; i < elements.length; i++) {
-
+    //gets the id of each of the sections present with class .card-body
     const name = elements[i].querySelector('.card-body').getAttribute('id');
+    //creates an li for each section and adds it to the navbar 
     const li = document.createElement('LI');
     li.insertAdjacentHTML("afterbegin", '<a class="nav-link"  > <span class= "fa fa-info fa-lg" ></span> ${name}</a > ');
     const a = li.querySelector('a');
+    //checks if its the welcome (home) to make it active
     if (name == "Welcome") {
         a.classList.add('active');
     }
@@ -21,23 +23,25 @@ for (let i = 0; i < elements.length; i++) {
 
 
 navbar.addEventListener('click', function (e) {
+    //loops on all li in navbar to remove active class
     const navbarNew = document.querySelectorAll('li');
-
     for (let i = 0; i < navbarNew.length; i++) {
-        console.log("class list " + navbarNew[i].classList);
+
         navbarNew[i].querySelector('a').classList.remove('active');
     }
+    //adds it to the target (what was clicked)
     e.target.classList.add('active');
     const n = e.target.getAttribute('data');
     const elm = document.getElementById(`${n}`);
-
-    elm.scrollIntoView();
+    //scrolls into the selected element 
+    elm.scrollIntoView({ behavior: "smooth" });
 
 });
 
+
+// return true if section is in view 
 function inView(elem) {
     const inV = elem.getBoundingClientRect();
-
     return (
         inV.top >= 0
         &&
@@ -48,10 +52,11 @@ function inView(elem) {
 
 
 document.addEventListener('scroll', function (e) {
+    //loops on all sections to check if its in view
     for (let i = 0; i < elements.length; i++) {
-
         const chk = elements[i];
         if (inView(chk)) {
+            // if in view adds "active" to it  corresponding li element class list and removes it from the others in navbar
             const ind = chk.getAttribute('id')
             const navbarNew = document.querySelectorAll('li');
             for (let i = 0; i < navbarNew.length; i++) {
@@ -68,26 +73,3 @@ document.addEventListener('scroll', function (e) {
 
 });
 
-
-
-//OLd method used to determine which element is in viewport by detemrining where the mouse is over
-
-// container.addEventListener('mouseover', function (e) {
-//     if (e.target.nodeName == "DIV") {
-//         const ind = e.target.getAttribute('id');
-//         console.log(ind);
-//         if (ind != 'top' || ind !== null || ind !== 'null') {
-//             const navbarNew = document.querySelectorAll('li');
-//             for (let i = 0; i < navbarNew.length; i++) {
-//                 if (navbarNew[i].querySelector('a').getAttribute('data') == ind) {
-//                     navbarNew[i].querySelector('a').classList.add('active');
-//                 }
-//                 else {
-//                     navbarNew[i].querySelector('a').classList.remove('active');
-//                 }
-//             }
-
-//         }
-//     }
-
-// });
--- readme.md
+++ readme.md
@@ -1,13 +1,19 @@
+  # **LandingPage**
+This is a simple landingpage created as a part of my Udacity Nanodegree, where the navbar is dynamically created in Javascript, and the section currently viewed is determined and destinguished.
+
+  # **Get Started**
 To run the project please open index.html file on your browser.
 
-Dependencies used: 
+  # ** Dependencies used: **
 -Bootstrap
 
-Resources used :
--MDN web docs
--JavascriptTutorial
+# **Resources used :**
+-[MDN web docs] (https://developer.mozilla.org/en-US/docs/Web/API/Element/scrollIntoView)
+-[JavascriptTutorial ] (https://www.javascripttutorial.net/dom/css/check-if-an-element-is-visible-in-the-viewport/)
 
-Description of functions:
+# **Description of functions: **
 - a.scrollIntoView() --> used to show element a in the user's current view
 - elem.getBoundingClientRect() --> returns the element's x,y position along with its wifth, height, top, right, bottom, left
 
+# **Author :** 
+Farah Ayoub
